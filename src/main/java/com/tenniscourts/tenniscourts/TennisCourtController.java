9140db5f41a4466f316bd76494a2576d9b2df400
cristicojo <crii_stii@yahoo.com>
2022-07-02 20:06:40 +0300

    refactor TennisCourtController

--- src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
+++ src/main/java/com/tenniscourts/tenniscourts/TennisCourtController.java
@@ -2,6 +2,7 @@ package com.tenniscourts.tenniscourts;
 
 import com.tenniscourts.config.BaseRestController;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +13,23 @@ import javax.validation.Valid;
 @RequestMapping(value = "/tennis", consumes = "application/json", produces = "application/json")
 public class TennisCourtController extends BaseRestController {
 
-    private final TennisCourtService tennisCourtService;
+	private final TennisCourtService tennisCourtService;
 
 
-    @PostMapping("/court")
-    public ResponseEntity<Void> addTennisCourt(@Valid @RequestBody TennisCourtDTO tennisCourtDTO) {
-        return ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
-    }
+	@PostMapping("/court")
+	public ResponseEntity<String> addTennisCourt(@Valid @RequestBody TennisCourtDTO tennisCourtDTO) {
+		ResponseEntity.created(locationByEntity(tennisCourtService.addTennisCourt(tennisCourtDTO).getId())).build();
+		return ResponseEntity.status(HttpStatus.CREATED).body("SUCCESS!");
 
-    @GetMapping("/court/{id}")
-    public ResponseEntity<TennisCourtDTO> findTennisCourtById(@PathVariable Long id) {
-        return ResponseEntity.ok(tennisCourtService.findTennisCourtById(id));
-    }
+	}
 
-    @GetMapping("/courtSchedules/{id}")
-    public ResponseEntity<TennisCourtDTO> findTennisCourtWithSchedulesById(@PathVariable Long id) {
-        return ResponseEntity.ok(tennisCourtService.findTennisCourtWithSchedulesById(id));
-    }
+	@GetMapping("/court/{id}")
+	public ResponseEntity<TennisCourtDTO> findTennisCourtById(@PathVariable Long id) {
+		return ResponseEntity.ok(tennisCourtService.findTennisCourtById(id));
+	}
+
+	@GetMapping("/courtSchedules/{id}")
+	public ResponseEntity<TennisCourtDTO> findTennisCourtWithSchedulesById(@PathVariable Long id) {
+		return ResponseEntity.ok(tennisCourtService.findTennisCourtWithSchedulesById(id));
+	}
 }
